0ae7fe21d181f24a11f09e9fbfd2a3e808bbda7c
Tobiasz-PC <tobiasz.kostrzewa@wp.pl>
2022-05-03 11:21:21 +0200

    16. Wygenerowanie szkieletu dokumentacji

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,25 +8,46 @@ public class DefaultCountingOutRhymer {
 
 	public int total = START_VALUE;
 
+	/**
+	 * Takes the int parameter and inserts into Rhymer
+	 * @param in
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * Checks if Rhymer is initialized
+	 * @return Boolean
+	 */
 	public boolean callCheck() {
 		return total == START_VALUE;
 	}
 
+	/**
+	 * Checks if Rhymer is full
+	 * @return Boolean
+	 */
 	public boolean isFull() {
 		return total == 11;
 	}
 
+
+	/**
+	 *
+	 * @return Last value in 'numbers'
+	 */
 	protected int peekaboo() {
 		if (callCheck())
 			return START_VALUE;
 		return numbers[total];
 	}
 
+	/**
+	 *
+	 * @return Last value and reduces total count
+	 */
 	public int countOut() {
 		if (callCheck())
 			return START_VALUE;
